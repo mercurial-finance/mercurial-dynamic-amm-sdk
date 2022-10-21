194b1bbadfe98be16203212fb6a2b928e3414cc3
McSam <mcsam1216@gmail.com>
2022-10-21 11:50:54 +0700

    fix: enable test

--- ts-client/src/amm/tests/index.test.ts
+++ ts-client/src/amm/tests/index.test.ts
@@ -165,28 +165,28 @@ describe('Interact with Devnet pool', () => {
     }
   });
 
-  // test('Swap SOL → mSOL', async () => {
-  //   const inAmountLamport = new BN(0.01 * 10 ** depegPool.tokenA.decimals);
+  test('Swap SOL → mSOL', async () => {
+    const inAmountLamport = new BN(0.01 * 10 ** depegPool.tokenA.decimals);
 
-  //   const quote = depegPool.getSwapQuote(new PublicKey(depegPool.tokenA.address), inAmountLamport, DEFAULT_SLIPPAGE);
-  //   expect(Number(quote)).toBeGreaterThan(0);
+    const quote = depegPool.getSwapQuote(new PublicKey(depegPool.tokenA.address), inAmountLamport, DEFAULT_SLIPPAGE);
+    expect(Number(quote)).toBeGreaterThan(0);
 
-  //   const swapTx = await depegPool.swap(
-  //     mockWallet.publicKey,
-  //     new PublicKey(depegPool.tokenA.address),
-  //     inAmountLamport,
-  //     quote,
-  //   );
+    const swapTx = await depegPool.swap(
+      mockWallet.publicKey,
+      new PublicKey(depegPool.tokenA.address),
+      inAmountLamport,
+      quote,
+    );
 
-  //   try {
-  //     const swapResult = await provider.sendAndConfirm(swapTx);
-  //     console.log('Swap Result of SOL → mSOL', swapResult);
-  //     expect(typeof swapResult).toBe('string');
-  //   } catch (error: any) {
-  //     console.trace(error);
-  //     throw new Error(error.message);
-  //   }
-  // });
+    try {
+      const swapResult = await provider.sendAndConfirm(swapTx);
+      console.log('Swap Result of SOL → mSOL', swapResult);
+      expect(typeof swapResult).toBe('string');
+    } catch (error: any) {
+      console.trace(error);
+      throw new Error(error.message);
+    }
+  });
 
   test('Swap mSOL → SOL', async () => {
     const inAmountLamport = new BN(0.01 * 10 ** depegPool.tokenB.decimals);
@@ -578,118 +578,118 @@ describe('Interact with Devnet pool', () => {
   });
 });
 
-// describe.only('Interact with Mainnet pool', () => {
-//   let stablePool: AmmImpl;
-//   let cpPool: AmmImpl;
-//   let depegPool: AmmImpl;
+describe.only('Interact with Mainnet pool', () => {
+  let stablePool: AmmImpl;
+  let cpPool: AmmImpl;
+  let depegPool: AmmImpl;
 
-//   beforeAll(async () => {
-//     const tokenListContainer = await new TokenListProvider().resolve();
-//     const tokenMap = tokenListContainer.filterByClusterSlug(MAINNET.cluster).getList();
+  beforeAll(async () => {
+    const tokenListContainer = await new TokenListProvider().resolve();
+    const tokenMap = tokenListContainer.filterByClusterSlug(MAINNET.cluster).getList();
 
-//     const USDT = tokenMap.find((token) => token.address === 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB');
-//     const USDC = tokenMap.find((token) => token.address === 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
-//     const SOL = tokenMap.find((token) => token.address === 'So11111111111111111111111111111111111111112');
-//     const STSOL = tokenMap.find((token) => token.address === '7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj');
+    const USDT = tokenMap.find((token) => token.address === 'Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB');
+    const USDC = tokenMap.find((token) => token.address === 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v');
+    const SOL = tokenMap.find((token) => token.address === 'So11111111111111111111111111111111111111112');
+    const STSOL = tokenMap.find((token) => token.address === '7dHbWXmci3dT8UFYWYZweBLXgycu7Y3iL6trKn1Y7ARj');
 
-//     cpPool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.USDC_SOL), USDC!, SOL!, {
-//       cluster: MAINNET.cluster as Cluster,
-//     });
-//     depegPool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.SOL_STSOL), SOL!, STSOL!, {
-//       cluster: MAINNET.cluster as Cluster,
-//     });
-//     stablePool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.USDT_USDC), USDC!, USDT!, {
-//       cluster: MAINNET.cluster as Cluster,
-//     });
-//   });
+    cpPool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.USDC_SOL), USDC!, SOL!, {
+      cluster: MAINNET.cluster as Cluster,
+    });
+    depegPool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.SOL_STSOL), SOL!, STSOL!, {
+      cluster: MAINNET.cluster as Cluster,
+    });
+    stablePool = await AmmImpl.create(MAINNET.connection, new PublicKey(MAINNET_POOL.USDT_USDC), USDC!, USDT!, {
+      cluster: MAINNET.cluster as Cluster,
+    });
+  });
 
-//   test('Get Pool Token Mint', () => {
-//     expect(cpPool.getPoolTokenMint()).toBeDefined();
-//     expect(depegPool.getPoolTokenMint()).toBeDefined();
-//     expect(stablePool.getPoolTokenMint()).toBeDefined();
-//   });
+  test('Get Pool Token Mint', () => {
+    expect(cpPool.getPoolTokenMint()).toBeDefined();
+    expect(depegPool.getPoolTokenMint()).toBeDefined();
+    expect(stablePool.getPoolTokenMint()).toBeDefined();
+  });
 
-//   test('Cpamm price impact', async () => {
-//     const inTokenMint = new PublicKey(cpPool.tokenA.address);
-//     const onePercentAmount = cpPool.poolInfo.tokenAAmount.div(new BN(100));
-//     const fiftyPercentAmount = cpPool.poolInfo.tokenAAmount.mul(new BN(50)).div(new BN(100));
-//     const oneHundredPercentAmount = cpPool.poolInfo.tokenAAmount;
+  test('Cpamm price impact', async () => {
+    const inTokenMint = new PublicKey(cpPool.tokenA.address);
+    const onePercentAmount = cpPool.poolInfo.tokenAAmount.div(new BN(100));
+    const fiftyPercentAmount = cpPool.poolInfo.tokenAAmount.mul(new BN(50)).div(new BN(100));
+    const oneHundredPercentAmount = cpPool.poolInfo.tokenAAmount;
 
-//     console.log('Pool token A amount', cpPool.poolInfo.tokenAAmount.toString());
-//     console.log('Pool token B amount', cpPool.poolInfo.tokenBAmount.toString());
+    console.log('Pool token A amount', cpPool.poolInfo.tokenAAmount.toString());
+    console.log('Pool token B amount', cpPool.poolInfo.tokenBAmount.toString());
 
-//     const [poolVaultALp, poolVaultBLp, vaultAReserve, vaultBReserve] = await Promise.all([
-//       MAINNET.connection.getTokenAccountBalance(cpPool.poolState.aVaultLp),
-//       MAINNET.connection.getTokenAccountBalance(cpPool.poolState.bVaultLp),
-//       MAINNET.connection.getTokenAccountBalance(cpPool.vaultA.vaultState.tokenVault),
-//       MAINNET.connection.getTokenAccountBalance(cpPool.vaultB.vaultState.tokenVault),
-//     ]);
+    const [poolVaultALp, poolVaultBLp, vaultAReserve, vaultBReserve] = await Promise.all([
+      MAINNET.connection.getTokenAccountBalance(cpPool.poolState.aVaultLp),
+      MAINNET.connection.getTokenAccountBalance(cpPool.poolState.bVaultLp),
+      MAINNET.connection.getTokenAccountBalance(cpPool.vaultA.vaultState.tokenVault),
+      MAINNET.connection.getTokenAccountBalance(cpPool.vaultB.vaultState.tokenVault),
+    ]);
 
-//     const swapQuoteParams = {
-//       currentTime: cpPool.poolInfo.currentTimestamp.toNumber(),
-//       depegAccounts: new Map(),
-//       poolState: cpPool.poolState,
-//       poolVaultALp: new BN(poolVaultALp.value.amount),
-//       poolVaultBLp: new BN(poolVaultBLp.value.amount),
-//       vaultA: cpPool.vaultA.vaultState,
-//       vaultB: cpPool.vaultB.vaultState,
-//       vaultALpSupply: cpPool.vaultA.lpSupply,
-//       vaultBLpSupply: cpPool.vaultB.lpSupply,
-//       vaultAReserve: new BN(vaultAReserve.value.amount),
-//       vaultBReserve: new BN(vaultBReserve.value.amount),
-//     };
+    const swapQuoteParams = {
+      currentTime: cpPool.poolInfo.currentTimestamp.toNumber(),
+      depegAccounts: new Map(),
+      poolState: cpPool.poolState,
+      poolVaultALp: new BN(poolVaultALp.value.amount),
+      poolVaultBLp: new BN(poolVaultBLp.value.amount),
+      vaultA: cpPool.vaultA.vaultState,
+      vaultB: cpPool.vaultB.vaultState,
+      vaultALpSupply: cpPool.vaultA.lpSupply,
+      vaultBLpSupply: cpPool.vaultB.lpSupply,
+      vaultAReserve: new BN(vaultAReserve.value.amount),
+      vaultBReserve: new BN(vaultBReserve.value.amount),
+    };
 
-//     const { priceImpact: priceImpact1 } = calculateSwapQuote(inTokenMint, onePercentAmount, swapQuoteParams);
-//     const { priceImpact: priceImpact2 } = calculateSwapQuote(inTokenMint, fiftyPercentAmount, swapQuoteParams);
-//     const { priceImpact: priceImpact3 } = calculateSwapQuote(inTokenMint, oneHundredPercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact1 } = calculateSwapQuote(inTokenMint, onePercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact2 } = calculateSwapQuote(inTokenMint, fiftyPercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact3 } = calculateSwapQuote(inTokenMint, oneHundredPercentAmount, swapQuoteParams);
 
-//     console.log(`Price impact with in amount ${onePercentAmount.toString()}`, priceImpact1);
-//     console.log(`Price impact with in amount ${fiftyPercentAmount.toString()}`, priceImpact2);
-//     console.log(`Price impact with in amount ${oneHundredPercentAmount.toString()}`, priceImpact3);
+    console.log(`Price impact with in amount ${onePercentAmount.toString()}`, priceImpact1);
+    console.log(`Price impact with in amount ${fiftyPercentAmount.toString()}`, priceImpact2);
+    console.log(`Price impact with in amount ${oneHundredPercentAmount.toString()}`, priceImpact3);
 
-//     expect(priceImpact3.toNumber()).toBeGreaterThan(priceImpact2.toNumber());
-//     expect(priceImpact2.toNumber()).toBeGreaterThan(priceImpact1.toNumber());
-//   });
+    expect(priceImpact3.toNumber()).toBeGreaterThan(priceImpact2.toNumber());
+    expect(priceImpact2.toNumber()).toBeGreaterThan(priceImpact1.toNumber());
+  });
 
-//   test('Ssamm price impact', async () => {
-//     const inTokenMint = new PublicKey(stablePool.tokenA.address);
-//     const onePercentAmount = stablePool.poolInfo.tokenAAmount.div(new BN(100));
-//     const fiftyPercentAmount = stablePool.poolInfo.tokenAAmount.mul(new BN(50)).div(new BN(100));
-//     const oneHundredPercentAmount = stablePool.poolInfo.tokenAAmount;
+  test('Ssamm price impact', async () => {
+    const inTokenMint = new PublicKey(stablePool.tokenA.address);
+    const onePercentAmount = stablePool.poolInfo.tokenAAmount.div(new BN(100));
+    const fiftyPercentAmount = stablePool.poolInfo.tokenAAmount.mul(new BN(50)).div(new BN(100));
+    const oneHundredPercentAmount = stablePool.poolInfo.tokenAAmount;
 
-//     console.log('Pool token A amount', stablePool.poolInfo.tokenAAmount.toString());
-//     console.log('Pool token B amount', stablePool.poolInfo.tokenBAmount.toString());
+    console.log('Pool token A amount', stablePool.poolInfo.tokenAAmount.toString());
+    console.log('Pool token B amount', stablePool.poolInfo.tokenBAmount.toString());
 
-//     const [poolVaultALp, poolVaultBLp, vaultAReserve, vaultBReserve] = await Promise.all([
-//       MAINNET.connection.getTokenAccountBalance(stablePool.poolState.aVaultLp),
-//       MAINNET.connection.getTokenAccountBalance(stablePool.poolState.bVaultLp),
-//       MAINNET.connection.getTokenAccountBalance(stablePool.vaultA.vaultState.tokenVault),
-//       MAINNET.connection.getTokenAccountBalance(stablePool.vaultB.vaultState.tokenVault),
-//     ]);
+    const [poolVaultALp, poolVaultBLp, vaultAReserve, vaultBReserve] = await Promise.all([
+      MAINNET.connection.getTokenAccountBalance(stablePool.poolState.aVaultLp),
+      MAINNET.connection.getTokenAccountBalance(stablePool.poolState.bVaultLp),
+      MAINNET.connection.getTokenAccountBalance(stablePool.vaultA.vaultState.tokenVault),
+      MAINNET.connection.getTokenAccountBalance(stablePool.vaultB.vaultState.tokenVault),
+    ]);
 
-//     const swapQuoteParams = {
-//       currentTime: stablePool.poolInfo.currentTimestamp.toNumber(),
-//       depegAccounts: new Map(),
-//       poolState: stablePool.poolState,
-//       poolVaultALp: new BN(poolVaultALp.value.amount),
-//       poolVaultBLp: new BN(poolVaultBLp.value.amount),
-//       vaultA: stablePool.vaultA.vaultState,
-//       vaultB: stablePool.vaultB.vaultState,
-//       vaultALpSupply: stablePool.vaultA.lpSupply,
-//       vaultBLpSupply: stablePool.vaultB.lpSupply,
-//       vaultAReserve: new BN(vaultAReserve.value.amount),
-//       vaultBReserve: new BN(vaultBReserve.value.amount),
-//     };
+    const swapQuoteParams = {
+      currentTime: stablePool.poolInfo.currentTimestamp.toNumber(),
+      depegAccounts: new Map(),
+      poolState: stablePool.poolState,
+      poolVaultALp: new BN(poolVaultALp.value.amount),
+      poolVaultBLp: new BN(poolVaultBLp.value.amount),
+      vaultA: stablePool.vaultA.vaultState,
+      vaultB: stablePool.vaultB.vaultState,
+      vaultALpSupply: stablePool.vaultA.lpSupply,
+      vaultBLpSupply: stablePool.vaultB.lpSupply,
+      vaultAReserve: new BN(vaultAReserve.value.amount),
+      vaultBReserve: new BN(vaultBReserve.value.amount),
+    };
 
-//     const { priceImpact: priceImpact1 } = calculateSwapQuote(inTokenMint, onePercentAmount, swapQuoteParams);
-//     const { priceImpact: priceImpact2 } = calculateSwapQuote(inTokenMint, fiftyPercentAmount, swapQuoteParams);
-//     const { priceImpact: priceImpact3 } = calculateSwapQuote(inTokenMint, oneHundredPercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact1 } = calculateSwapQuote(inTokenMint, onePercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact2 } = calculateSwapQuote(inTokenMint, fiftyPercentAmount, swapQuoteParams);
+    const { priceImpact: priceImpact3 } = calculateSwapQuote(inTokenMint, oneHundredPercentAmount, swapQuoteParams);
 
-//     console.log(`Price impact with in amount ${onePercentAmount.toString()}`, priceImpact1);
-//     console.log(`Price impact with in amount ${fiftyPercentAmount.toString()}`, priceImpact2);
-//     console.log(`Price impact with in amount ${oneHundredPercentAmount.toString()}`, priceImpact3);
+    console.log(`Price impact with in amount ${onePercentAmount.toString()}`, priceImpact1);
+    console.log(`Price impact with in amount ${fiftyPercentAmount.toString()}`, priceImpact2);
+    console.log(`Price impact with in amount ${oneHundredPercentAmount.toString()}`, priceImpact3);
 
-//     expect(priceImpact3.toNumber()).toBeGreaterThan(priceImpact2.toNumber());
-//     expect(priceImpact2.toNumber()).toBeGreaterThan(priceImpact1.toNumber());
-//   });
-// });
+    expect(priceImpact3.toNumber()).toBeGreaterThan(priceImpact2.toNumber());
+    expect(priceImpact2.toNumber()).toBeGreaterThan(priceImpact1.toNumber());
+  });
+});
