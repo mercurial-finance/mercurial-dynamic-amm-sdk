f047518683a6c7afc0c94912916c4b87cf33c2e1
codewithgun <codewithgun@gmail.com>
2023-04-19 13:18:50 +0800

    add bootstrap liquidity, update ts idl

--- ts-client/src/amm/types/index.ts
+++ ts-client/src/amm/types/index.ts
@@ -5,6 +5,7 @@ import BN from 'bn.js';
 import { Amm as AmmIdl } from '../idl';
 import { VaultState, VaultIdl } from '@mercurial-finance/vault-sdk';
 import Decimal from 'decimal.js';
+import { publicKey, struct, u64, u8, option } from '@project-serum/borsh';
 
 export type AmmProgram = Program<AmmIdl>;
 export type VaultProgram = Program<VaultIdl>;
@@ -157,6 +158,46 @@ export type AccountsInfo = {
   poolLpSupply: BN;
   currentTime: BN;
 };
+
+export interface StakePool {
+  totalLamports: BN;
+  poolTokenSupply: BN;
+}
+
+const feeFields = [u64('denominator'), u64('numerator')];
+
+export const StakePoolLayout = struct([
+  u8('accountType'),
+  publicKey('manager'),
+  publicKey('staker'),
+  publicKey('stakeDepositAuthority'),
+  u8('stakeWithdrawBumpSeed'),
+  publicKey('validatorList'),
+  publicKey('reserveStake'),
+  publicKey('poolMint'),
+  publicKey('managerFeeAccount'),
+  publicKey('tokenProgramId'),
+  u64('totalLamports'),
+  u64('poolTokenSupply'),
+  u64('lastUpdateEpoch'),
+  struct([u64('unixTimestamp'), u64('epoch'), publicKey('custodian')], 'lockup'),
+  struct(feeFields, 'epochFee'),
+  option(struct(feeFields), 'nextEpochFee'),
+  option(publicKey(), 'preferredDepositValidatorVoteAddress'),
+  option(publicKey(), 'preferredWithdrawValidatorVoteAddress'),
+  struct(feeFields, 'stakeDepositFee'),
+  struct(feeFields, 'stakeWithdrawalFee'),
+  option(struct(feeFields), 'nextStakeWithdrawalFee'),
+  u8('stakeReferralFee'),
+  option(publicKey(), 'solDepositAuthority'),
+  struct(feeFields, 'solDepositFee'),
+  u8('solReferralFee'),
+  option(publicKey(), 'solWithdrawAuthority'),
+  struct(feeFields, 'solWithdrawalFee'),
+  option(struct(feeFields), 'nextSolWithdrawalFee'),
+  u64('lastEpochPoolTokenSupply'),
+  u64('lastEpochTotalLamports'),
+]);
 
 /** Utils */
 export interface ParsedClockState {
