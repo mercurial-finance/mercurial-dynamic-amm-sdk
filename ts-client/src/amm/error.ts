2a2c7f2c335d199dbe605d4514f01a348e143a8b
McSam <mcsam1216@gmail.com>
2023-05-07 20:24:31 +0800

    fix: conditional constructor

--- ts-client/src/amm/error.ts
+++ ts-client/src/amm/error.ts
@@ -1,20 +1,39 @@
+import { createProgram } from './utils';
+import { IDL, Amm } from '../amm/idl';
+
+type Codes = (typeof IDL.errors)[number]['code'];
+
 class DynamicAmmError extends Error {
-  public errorNumber: number;
-  public errorCode: string;
-  public errorDescription;
+  public errorCode: number;
+  public errorName: string;
+  public errorMessage: string;
 
-  constructor(error: object) {
-    const logs = JSON.parse(JSON.stringify(error));
-    const errorLog = logs.find((log) => log.includes('AnchorError'));
-    const errorNumber = errorLog.match(/Error Number: (\d+)/)[1];
-    const errorCode = errorLog.match(/Error Code: (\w+)/)[1];
-    const errorDescription = errorLog.match(/Error Message: (\w+)/)[1];
+  constructor(error: object | Codes) {
+    let _errorCode = 0;
+    let _errorName = 'Something went wrong';
+    let _errorMessage = 'Something went wrong';
 
-    super(errorDescription);
+    if (typeof error !== 'object') {
+      const error = IDL.errors.find((err) => err.code === error);
 
-    this.errorNumber = errorNumber;
-    this.errorCode = errorCode;
-    this.errorDescription = errorDescription;
+      if (error) {
+        _errorCode = error.code;
+        _errorName = error.name;
+        _errorMessage = error.msg;
+      }
+    } else {
+      const { logs } = JSON.parse(JSON.stringify(error));
+      const errorLog = logs.find((log) => log.includes('AnchorError'));
+      _errorCode = errorLog.match(/Error Number: (\d+)/)[1];
+      _errorName = errorLog.match(/Error Code: (\w+)/)[1];
+      _errorMessage = errorLog.match(/Error Message: (.*?)(?=\.)/)[1];
+    }
+
+    super(_errorMessage);
+
+    this.errorCode = _errorCode;
+    this.errorName = _errorName;
+    this.errorMessage = _errorMessage;
   }
 }
 
