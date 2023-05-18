edfbe06ba3825c648f1142ec377602e03322ff68
McSam <mcsam1216@gmail.com>
2023-05-18 12:47:17 +0800

    export fee bps constant

--- ts-client/index.ts
+++ ts-client/index.ts
@@ -1,7 +1,14 @@
 import { VaultIdl as Vault, IDL as VaultIdl } from '@mercurial-finance/vault-sdk';
 import AmmImpl from './src/amm';
 import { StableSwap, ConstantProductSwap } from './src/amm/curve';
-import { PROGRAM_ID, MAINNET_POOL, DEVNET_POOL, CURVE_TYPE_ACCOUNTS } from './src/amm/constants';
+import {
+  PROGRAM_ID,
+  MAINNET_POOL,
+  DEVNET_POOL,
+  CURVE_TYPE_ACCOUNTS,
+  CONSTANT_PRODUCT_ALLOWED_TRADE_FEE_BPS,
+  STABLE_SWAP_ALLOWED_TRADE_FEE_BPS,
+} from './src/amm/constants';
 import {
   getOnchainTime,
   calculateMaxSwapOutAmount,
@@ -33,6 +40,8 @@ export {
   MAINNET_POOL,
   DEVNET_POOL,
   CURVE_TYPE_ACCOUNTS,
+  CONSTANT_PRODUCT_ALLOWED_TRADE_FEE_BPS,
+  STABLE_SWAP_ALLOWED_TRADE_FEE_BPS,
   // IDL
   AmmIdl,
   VaultIdl,
