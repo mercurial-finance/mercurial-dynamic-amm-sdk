2b6b42d258b83154c4a0d464fb2620de563a974a
quangkeu95 <quangkeu95@gmail.com>
2024-01-04 09:59:10 +0700

    feat: Add PoolCreated event

--- programs/amm/src/event.rs
+++ programs/amm/src/event.rs
@@ -1,10 +1,12 @@
 //! Event module includes information about events of the program
 use anchor_lang::prelude::*;
+use crate::curve::fees::PoolFees;
+use crate::curve::curve_type::CurveType;
+use crate::state::{Padding, PoolType};
 
 /// Add liquidity event
 #[event]
 #[derive(Debug, Clone, Copy)]
-
 pub struct AddLiquidity {
     /// LP amount user received upon add liquidity.
     pub lp_mint_amount: u64,
@@ -17,7 +19,6 @@ pub struct AddLiquidity {
 /// Remove liquidity event
 #[event]
 #[derive(Debug, Clone, Copy)]
-
 pub struct RemoveLiquidity {
     /// LP amount burned from user upon add remove liquidity.
     pub lp_unmint_amount: u64,
@@ -30,7 +31,6 @@ pub struct RemoveLiquidity {
 /// Swap event
 #[event]
 #[derive(Debug, Clone, Copy)]
-
 pub struct Swap {
     /// Token amount user deposited to the pool for token exchange.
     pub in_amount: u64,
@@ -56,4 +56,44 @@ pub struct PoolInfo {
     pub virtual_price: f64,
     /// Current unix timestamp
     pub current_timestamp: u64,
+}
+
+/// New pool created event
+#[event]
+pub struct PoolCreated {
+    /// LP token mint of the pool
+    pub lp_mint: Pubkey, //32
+    /// Token A mint of the pool. Eg: USDT
+    pub token_a_mint: Pubkey, //32
+    /// Token B mint of the pool. Eg: USDC
+    pub token_b_mint: Pubkey, //32
+    /// Vault account for token A. Token A of the pool will be deposit / withdraw from this vault account.
+    pub a_vault: Pubkey, //32
+    /// Vault account for token B. Token B of the pool will be deposit / withdraw from this vault account.
+    pub b_vault: Pubkey, //32
+    /// LP token account of vault A. Used to receive/burn the vault LP upon deposit/withdraw from the vault.
+    pub a_vault_lp: Pubkey, //32
+    /// LP token account of vault B. Used to receive/burn the vault LP upon deposit/withdraw from the vault.
+    pub b_vault_lp: Pubkey, //32
+    /// "A" vault lp bump. Used to create signer seeds.
+    pub a_vault_lp_bump: u8, //1
+    /// Flag to determine whether the pool is enabled, or disabled.
+    pub enabled: bool, //1
+    /// Admin fee token account for token A. Used to receive trading fee.
+    pub admin_token_a_fee: Pubkey, //32
+    /// Admin fee token account for token B. Used to receive trading fee.
+    pub admin_token_b_fee: Pubkey, //32
+    /// Owner of the pool.
+    pub admin: Pubkey, //32
+    /// Store the fee charges setting.
+    pub fees: PoolFees, //48
+    /// Pool type
+    pub pool_type: PoolType,
+    /// Stake pubkey of SPL stake pool
+    pub stake: Pubkey,
+    /// Padding for future pool field
+    pub padding: Padding, // 512 Refer: curve_type.rs for the test
+    /// The type of the swap curve supported by the pool.
+    // Leaving curve_type as last field give us the flexibility to add specific curve information / new curve type
+    pub curve_type: CurveType, //9
 }
